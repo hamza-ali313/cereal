f713c78b0cf64914a2d859dccf748032d947fee2
hamza ali <hamzabinsultan313@gmail.com>
2024-07-22 21:10:51 +0500

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+<style>
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+    }
+</style>
+<script src="js/jquery-3.6.3.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/fancybox.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="https://cdn.ckeditor.com/ckeditor5/40.1.0/classic/ckeditor.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.0.0/cropper.min.js"></script>
+<script type="text/javascript" src="js/slick.js"></script>
+<script src="js/custom.js"></script>
+<script src="croppie.js"></script>
+<script type="text/javascript">
+    // // custom cursor script start
+    // const cursor = document.querySelector('.cursor');
+    // document.addEventListener('mousemove', e => {
+    //     cursor.setAttribute("style", "top: " + (e.pageY - 3) + "px; left: " + (e.pageX - 3) + "px;")
+    // })
+
+</script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,125 @@
+<?php
+$page = 'Home';
+include 'includes/header.php';
+$page = 'home';
+?>
+
+
+
+<!-- section index pg start -->
+<section class='index-pg'>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-12 col-xl-3 col-lg-4">
+                <div class='img1'>
+                    <img src="images/img1.png" alt="">
+                </div>
+            </div>
+            <div class="col-12 col-xl-6 col-lg-4">
+                <div class='text-center'>
+                    <h2>Is launching on kickstarter</h2>
+                </div>
+            </div>
+            <div class="col-12 col-xl-3 col-lg-4">
+                <div class='img3'>
+                    <img src="images/img3.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-xl-3 col-lg-4">
+                <div class='img4'>
+                    <a href="images/vid.mp4" data-fancybox><img src="images/img4.png" alt=""></a>
+                </div>
+                <div class='img2'>
+                    <!-- <video controls>
+                        <source src="images/vid.mp4" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video> -->
+                </div>
+            </div>
+            <div class="col-12 col-xl-6 col-lg-4">
+                <div class="Cerealbox">
+                    <img src="images/Cerealbox.png" alt="">
+                </div>
+            </div>
+            <div class="col-12 col-xl-3 col-lg-4">
+                <div class='img5'>
+                    <img src="images/img5.png" alt="">
+                </div>
+                <div class='img6'>
+                    <img src="images/img6.png" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-xl-3 col-lg-4 col-md-12">
+                <div class="row align-items-center my-md-5 ">
+                    <div class="col-12 col-md-6 col-lg-12">
+                        <div class='facebook'>
+                            <a href="https://www.facebook.com/cerealclo?mibextid=JRoKGi" class="btn1"
+                                target="blank">facebook</a>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-12">
+                        <div class='Instagram mt-lg-5'>
+                            <a href="https://www.instagram.com/cereal_clothing/" target="blank"
+                                class="btn1">Instagram</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-xl-6 col-lg-4 col-md-12">
+                <div class="boom">
+                    <img src="images/boom.png" alt="">
+                </div>
+            </div>
+            <div class="col-12 col-xl-3 col-lg-4 col-md-12">
+                <div class='Instruction'>
+                    <img src="images/Instruction.png" alt="">
+                </div>
+                <div class='Email mb-4'>
+                    <form action="">
+                        <div class='email'>
+                            <label for="">email</label>
+                            <input type="text">
+                        </div>
+                        <button class='btn1'>
+                            submit
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class='text-center py-5 expertise-count'>
+                    <h2 id="countdown">
+                        the countdown has begun:
+                        <span class="days count" data-target="2024-04-17T00:00:00"></span> Days
+                        <span class="hours count" data-target="2024-04-17T00:00:00"></span> hrs.
+                        <span class="minutes count" data-target="2024-04-17T00:00:00"></span> min
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- section index pg end -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include 'includes/footer.php';
+$page = 'home';
+?>
